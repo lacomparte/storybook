46d5c9172d482da69838faedcde9ba223ce77b6e
김상락/프론트개발팀 <ksrrr.rak@musinsa.com>
2021-08-30 11:50:27 +0900

    index.js 수정

--- src/index.js
+++ src/index.js
@@ -2,3 +2,7 @@ export * from "./components/tabs/Tab";
 export * from "./components/tabs/TabParent";
 export * from "./components/segmentControls/SegmentControl";
 export * from "./components/segmentControls/SegmentControlParent";
+
+export function Test() {
+  console.log(123);
+}
