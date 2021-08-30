f4dfb11df852b03b0a18bd3afb87616c6b33d1c4
김상락/프론트개발팀 <ksrrr.rak@musinsa.com>
2021-08-30 11:42:54 +0900

    index.js 수정

--- src/index.js
+++ src/index.js
@@ -1,20 +1,4 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 export * from "./components/tabs/Tab";
 export * from "./components/tabs/TabParent";
 export * from "./components/segmentControls/SegmentControl";
 export * from "./components/segmentControls/SegmentControlParent";
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
