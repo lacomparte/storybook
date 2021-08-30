1f6a4ec47d8d34ea8982b75f989cb779c05529b9
김상락/프론트개발팀 <ksrrr.rak@musinsa.com>
2021-08-30 12:08:21 +0900

    @type 정의

--- src/@types/index.d.ts
+++ src/@types/index.d.ts
@@ -1,1 +1,31 @@
-declare module "*.mdx";
+declare module "*.ts";
+
+export interface StyledTapButton {
+  active: boolean;
+  handleClickButton?(e: React.SyntheticEvent<HTMLButtonElement>): void;
+}
+export interface TabProps {
+  id: number;
+  label: string;
+  handleClickButton?: (id: number) => {};
+  activeTab?: number;
+  rest?: any;
+}
+
+export interface StyledSegmentButton {
+  active: boolean;
+  addStyle: AddStyle;
+}
+
+export interface AddStyle {
+  [index: string]: string;
+}
+export interface SegmentControlProps {
+  id?: number;
+  label: string;
+  count: number;
+  handleClickButton?: (id: number) => {};
+  activeTab?: number;
+  addStyle?: AddStyle;
+  rest?: any;
+}
--- src/index.js
+++ src/index.js
@@ -2,7 +2,3 @@ export * from "./components/tabs/Tab";
 export * from "./components/tabs/TabParent";
 export * from "./components/segmentControls/SegmentControl";
 export * from "./components/segmentControls/SegmentControlParent";
-
-export function Test() {
-  console.log(123);
-}
