d9d7f4d4b735675b591faaadeb90dd13a2bac425
김상락/프론트개발팀 <ksrrr.rak@musinsa.com>
2021-08-30 16:04:40 +0900

    tsconfig paths webpack plugin 설정

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
-export * from "./components/tabs/Tab";
-export * from "./components/tabs/TabParent";
-export * from "./components/segmentControls/SegmentControl";
-export * from "./components/segmentControls/SegmentControlParent";
+// export * from "./components/tabs/Tab";
+// export * from "./components/tabs/TabParent";
+// export * from "./components/segmentControls/SegmentControl";
+// export * from "./components/segmentControls/SegmentControlParent";
+
+function Test() {
+  console.log(123);
+}
+
+export default Test;
