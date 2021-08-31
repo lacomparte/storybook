4c2934a6c86cf29cf562f5999b349292daacffe3
김상락/프론트개발팀 <ksrrr.rak@musinsa.com>
2021-08-31 14:06:27 +0900

    babelrc 추가

--- src/index.js
+++ src/index.js
@@ -1,10 +1,4 @@
-// export * from "./components/tabs/Tab";
-// export * from "./components/tabs/TabParent";
-// export * from "./components/segmentControls/SegmentControl";
-// export * from "./components/segmentControls/SegmentControlParent";
-
-function Test() {
-  console.log(123);
-}
-
-export default Test;
+export * from "./components/tabs/Tab";
+export * from "./components/tabs/TabParent";
+export * from "./components/segmentControls/SegmentControl";
+export * from "./components/segmentControls/SegmentControlParent";
